97e87a595823f0bb2381da23341cbf8b962754bc
MatthewlWilcox <142836648+MatthewLeeWilcox@users.noreply.github.com>
2023-10-12 13:43:52 -0700

    continue adding to school names
    continue ading to school names. Adjust the shrunk data to include the locale variable not locale2

--- Validate data.R
+++ Validate data.R
@@ -5,7 +5,7 @@ library(data.table)
 current_directory <- getwd()
 data_path <- '/Data/Shrunk_data.csv'
 data_path_raw <- '/Data/RAW_DATA/MERGED2021_22_PP.csv'
-
+l
 df_raw <-  fread(paste(current_directory, data_path_raw, sep = ""))
 df <- fread(paste(current_directory, data_path, sep = ""))
 
@@ -16,12 +16,16 @@ df_raw[df_raw == "NULL"] <- NA
 
 # Use Column Means to get the Percentage of the values that are not NA.
 # ie 100% means no missing values
+
+# Use to select ony certain columns to check
+df_raw<- df_raw %>% select(LOCALE, LOCALE2)
+
 pct_variables_included <- colMeans(!is.na(df)) *100
 pct_variables_included_raw <- colMeans(!is.na(df_raw))*100
 
 
 # Return a DF that shows percentage of values that are not NA. 
-df <- data.frame(
+df_missing <- data.frame(
   variable_name = colnames(df_raw),
   pct_included = pct_variables_included_raw
 )
--- school_name_compare.R
+++ school_name_compare.R
@@ -2,34 +2,91 @@ library(tidyverse)
 library(data.table)
 library(usdata)
 library(stringr)
+library(ggplot2)
+library(usmap)
+library(maps)
+library(mapdata)
 # Data directory
 current_directory <- getwd()
 data_path <- '/Data/Shrunk_data.csv'
 
 # Read in Data
-df <- fread(paste(current_directory, data_path, sep = ""))
+df_shrink <- fread(paste(current_directory, data_path, sep = ""))
 
-df[df == "Null"]<- NA
+# Make Null NA
+df_shrink[df_shrink == "Null"]<- NA
 
-uni_names_df <- df %>% select('INSTNM', 'CITY', 'STABBR', 'ZIP') %>%
+# Select columns Used and Rename
+uni_names_df <- df_shrink %>%
   rename('instution' = 'INSTNM',
          'city' = 'CITY', 
          'state_abbr' = 'STABBR',
          'zip' = 'ZIP')
 
+
+# Create a vector of all univeristys names
 uni_names_df$vector_name<- strsplit(uni_names_df$instution, split = " ")
 
 
+# Map the State Abbreviations to actual State Names and create columm state_names
 state_mapping <- setNames(state.name, state.abb)
 
 uni_names_df$state_name <- state_mapping[uni_names_df$state_abbr]
 
+# Build Boolean Variable state_name_included if state name is in the university name
 uni_names_df$state_name_included <- str_detect(tolower(uni_names_df$instution),tolower(uni_names_df$state_name))
 
+# Build Boolean Variable city_name_included if city is in the university name
 uni_names_df$city_name_included <- str_detect(tolower(uni_names_df$instution), tolower(uni_names_df$city))
 
 
-uni_names_df %>% group_by(state_abbr, city, city_name_included) %>% 
+
+# Count of total colleges per city
+total_colleges_per_city <- uni_names_df %>% group_by(state_abbr, city) %>%
+  summarise(total_count=n())%>% arrange(desc(total_count))
+
+total_colleges_per_city_20 <- head(total_colleges_per_city, 20)
+# Graph previous
+
+ggplot(data = total_colleges_per_city_20, aes(x = reorder(city, total_count), y = total_count)) + 
+  geom_bar(stat = "identity") +
+  coord_flip() + 
+  labs(
+    title = "Number of College in Each City"
+  ) + 
+  xlab("City") + 
+  ylab("Number of Colleges")
+
+
+# Group by the city and if the city name is included and count.
+city_name_groupby <- uni_names_df %>% group_by(state_abbr, city, city_name_included) %>% 
   summarise(total_count=n(),
             .groups = 'drop')%>% arrange(desc(total_count))
+
+# Group by the State 
+state_name_groupby <- uni_names_df %>% group_by(state_abbr, state_name_included) %>% 
+  summarise(total_count=n(),
+            .groups = 'drop')%>% arrange(desc(total_count)) %>%
+  filter(state_name_included == TRUE)%>% 
+  rename('state' = 'state_abbr')
+
+# Plot States
+# states_map <- map_data("state")
+# state_name_groupby$region <- tolower(state_mapping[state_name_groupby$state_abbr])
+# state_name_groupby_map <- left_join(states_map, state_name_groupby, by = "region")
+# 
+# ggplot(state_name_groupby_map, aes(long, lat, group = group))+
+#   geom_polygon(aes(fill = total_count)) 
+
+
+plot_usmap(data = state_name_groupby, values = "total_count") + 
+  labs(
+    title = "Number of College with State Name in University Name",
+    fill = "Number of Colleges"
+  )
+
+
+
+
+
 
